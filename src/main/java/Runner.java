7f88425d5cdae76304e4c25ccac53eff01449423
Emre MENTES <emreowen57@gmail.com>
2025-09-26 10:19:48 +0200

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //Version 1
+        System.out.println("Hello Git");
+        System.out.println("git add .: Staging area ya alir");
+    }
+}
